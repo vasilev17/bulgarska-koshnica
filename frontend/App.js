88512a9ae3e1698dd7f8b6c7bb953ac0b3019721
Valentin Vasilev <VVVasilev@nemetschek.bg>
2024-02-29 00:10:10 +0200

    frontend react native setup with expo

---
+++ frontend/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>BG Кошница!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: 'cyan',
+    fontSize: 24,
+  },
+});
