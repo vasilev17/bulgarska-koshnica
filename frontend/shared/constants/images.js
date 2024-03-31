5fa2b2fcafa571f309d0dba7f2836486ac19c91c
Valentin Vasilev <valio_vv@abv.bg>
2024-03-31 17:10:34 +0300

    merge

---
+++ frontend/shared/constants/images.js
@@ -0,0 +1,21 @@
+import headerWave from "../../assets/images/headerWave.png";
+import footerWaveEmpty from "../../assets/images/footerWaveEmpty.png";
+import footerWaveWithContent from "../../assets/images/footerWaveWithContent.png";
+import basketLogo from "../../assets/images/basket.png";
+import buyer from "../../assets/images/buyer.png";
+import producer from "../../assets/images/producer.png";
+import standardMap from "../../assets/images/standardMap.png";
+import satelliteMap from "../../assets/images/satelliteMap.png";
+
+
+
+export default {
+    headerWave,
+    footerWaveEmpty,
+    footerWaveWithContent,
+    basketLogo,
+    buyer,
+    producer,
+    standardMap,
+    satelliteMap,
+};
