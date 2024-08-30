562d23a5ecbae04fe2228af44167536226553eb4
callmeFilip <filip_andonow@abv.bg>
2024-08-30 23:23:32 +0300

    [Update] Added note for future improvement of user-not-found-exception

--- backend/error_handling/exception_definitions/user-not-found-exception.js
+++ backend/error_handling/exception_definitions/user-not-found-exception.js
@@ -1,5 +1,6 @@
 const BaseException = require("./baseException");
 
+//TODO inherit NOT FOUND EXCEPTION
 class UserNotFoundException extends BaseException {
   constructor() {
     super(404, "User not found");
