22191356494f5746205fd87f9942ea858f22b61d
callmeFilip <filip_andonow@abv.bg>
2024-08-31 22:23:14 +0300

    [Add] Added implementation of GetSchedule API endpoint

--- backend/utils/storage.js
+++ backend/utils/storage.js
@@ -194,6 +194,20 @@ async function getDeliveryPosInfo(location_id) {
 
   return rows[0];
 }
+
+async function getSchedule(location_id) {
+  const [rows] = await db.executeQuery(
+    `SELECT schedule FROM bulgarska_koshnica.locations WHERE location_id = ?;`,
+    [location_id]
+  );
+
+  if (rows[0] === undefined) {
+    throw new LocationNotFoundException();
+  }
+
+  return rows[0];
+}
+
 async function getLocationKeyWords(location_id) {
   const [rows] = await db.executeQuery(
     `SELECT keywords FROM bulgarska_koshnica.locations WHERE location_id = ?;`,
@@ -220,5 +234,6 @@ module.exports = {
   getLocationInfo,
   getContacts,
   getDeliveryPosInfo,
+  getSchedule,
   getLocationKeyWords,
 };
